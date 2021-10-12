2fec7d496f8a1bfa6fc0d36f8a84ff21d323ec96
Hamza <aboulashamza@gmail.com>
2021-10-12 15:05:30 +0200

    sql config

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,3 +1,12 @@
 <?php 
 
-$conn= new mysqli('localhost','root','','fos_db')or die("Could not connect to mysql".mysqli_error($con));
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+
+$conn= new mysqli($cleardb_server,$cleardb_username,$cleardb_password,$cleardb_db)or die("Could not connect to mysql".mysqli_error($con));
