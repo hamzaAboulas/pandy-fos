cf283ca90a3cf84ac518d2938671363645daaa15
Hamza <aboulashamza@gmail.com>
2021-10-12 15:42:59 +0200

    trying to display the users

--- admin/users.php
+++ admin/users.php
@@ -61,6 +61,56 @@
 			</div>
 		</div>
 	</div>
+	<div class="row">
+		<div class="card col-lg-12">
+			<div class="card-body">
+				<table class="table-striped table-bordered col-md-12">
+			<thead>
+				<tr>
+					<th class="text-center">#</th>
+					<th class="text-center">Name</th>
+					<th class="text-center">Username</th>
+				</tr>
+			</thead>
+			<tbody>
+				<?php
+ 					include 'db_connect.php';
+ 					$users = $conn->query("SELECT * FROM user_info order by name asc");
+ 					$i = 1;
+ 					while($row= $users->fetch_assoc()):
+				 ?>
+				 <tr>
+				 	<td>
+				 		<?php echo $i++ ?>
+				 	</td>
+				 	<td>
+				 		<?php echo $row['first_name'] ?>
+				 	</td>
+				 	<td>
+				 		<?php echo $row['last_name'] ?>
+				 	</td>
+				 	<td>
+				 		<center>
+								<div class="btn-group">
+								  <button type="button" class="btn btn-primary">Action</button>
+								  <button type="button" class="btn btn-primary dropdown-toggle dropdown-toggle-split" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+								    <span class="sr-only">Toggle Dropdown</span>
+								  </button>
+								  <div class="dropdown-menu">
+								    <a class="dropdown-item edit_user" href="javascript:void(0)" data-id = '<?php echo $row['id'] ?>'>Edit</a>
+								    <div class="dropdown-divider"></div>
+								    <a class="dropdown-item delete_user" href="javascript:void(0)" data-id = '<?php echo $row['id'] ?>'>Delete</a>
+								  </div>
+								</div>
+								</center>
+				 	</td>
+				 </tr>
+				<?php endwhile; ?>
+			</tbody>
+		</table>
+			</div>
+		</div>
+	</div>
 
 </div>
 <script>
